6b4e1d60093116bb82f307ec9306a351dd06e5e5
Venkat <ramanacs09@gmail.com>
2016-12-09 17:26:58 +0530

    Initial Commit

---
+++ AddTwoIntegers.java
@@ -0,0 +1,13 @@
+/**
+* Add Two integers
+*/
+
+public class AddTwoIntegers{
+	public static void main(String[] args){
+		int num1, num2;
+		num1 = 10;
+		num2 = 20;
+		
+		System.out.println("Sum of 10 and 20 is:"+(10+20));
+	}
+}
